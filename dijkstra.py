89b3235c6c7d5f85a870e2e2f03a224d330f3e80
Walter <walter.gordy@gmail.com>
2022-04-03 10:51:16 -0600

    Builds graph from grid

--- dijkstra.py
+++ dijkstra.py
@@ -1,115 +1,122 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov 12 10:25:02 2020
+using System;
+using System.Collections.Generic;
 
-@author: walter
-"""
+namespace PathFinding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = new PathFinder();
 
-import sys
-import math
-import numpy as np
-import random
-import time
-import copy
+            var grid = path.BlankGrid(5, 5);
+            var graph = path.BuildGraphFromGrid(grid);
+            path.PrintGraph(graph);
+            Console.ReadLine();
+        }
+    }
 
+    class PathFinder
+    {
+        public Dictionary<int, List<int[]>> BuildGraphFromGrid(int[][] grid)
+        {
+            if (grid == null || grid.Length == 0)
+                throw new ArgumentException("Grid must be defined");
+            else
+                for (int i = 0; i < grid.Length; i++)
+                    if (grid[0].Length != grid[i].Length)
+                        throw new ArgumentException("Grid must be rectangular");
 
-def print_grid(grid_to_draw):
-    for ry in grid_to_draw:
-        for cx in ry:
-            print(int(cx), end=" ")
-        print("")
+            var graph = new Dictionary<int, List<int[]>>();
 
+            var height = grid.Length;
+            var width = grid[0].Length;
+            var gridNodes = CreateGridNodes(height, width);
 
-def build_np_graph(grid):
-    graph = {}
-    height = grid.shape[0]
-    width = grid.shape[1]
-    grid_nodes = np.arange(width * height).reshape(grid.shape)
-    for i in range(height):
-        for j in range(width):
-            node = grid_nodes[i,j]
-            child_nodes = {}
-            mul = 1
-            if i - 1 >= 0:
-                if grid[i-1, j] == 0:
-                    child_nodes[grid_nodes[i-1, j]] = mul
-            if i + 1 < height:
-                if grid[i+1, j] == 0:
-                    child_nodes[grid_nodes[i+1, j]] = mul
-            if j - 1 >= 0:
-                if grid[i, j-1] == 0:
-                    child_nodes[grid_nodes[i, j-1]] = mul
-            if j + 1 < width:
-                if grid[i, j+1] == 0:
-                    child_nodes[grid_nodes[i, j+1]] = mul
-            graph[node] = child_nodes
-    return graph
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    var node = gridNodes[i][j];
+                    var childNodes = new List<int[]>();
+                    var mul = 1;
+                    if (i - 1 >= 0)
+                        if (grid[i - 1][j] == 0)
+                            childNodes.Add(new int[] { gridNodes[i - 1][j], mul });
+                    if (i + 1 < height)
+                        if (grid[i + 1][j] == 0)
+                            childNodes.Add(new int[] { gridNodes[i + 1][j], mul });
+                    if (j - 1 >= 0)
+                        if (grid[i][j - 1] == 0)
+                            childNodes.Add(new int[] { gridNodes[i][j - 1], mul });
+                    if (j + 1 < width)
+                        if (grid[i][j + 1] == 0)
+                            childNodes.Add(new int[] {gridNodes[i][j + 1], mul});
+                    graph.Add(node, childNodes);
+                }
+            }
+            return graph;
+        }
 
-def dijkstra(graph,start,goal):
-    shortest_distance = {}
-    predecessor = {}
-    unseenNodes = graph
-    infinity = 100
-    path = []
+        protected int[][] CreateGridNodes(int height, int width)
+        {
+            var gridNodes = new int[height][];
+            var gridIndex = 0;
+            for (int i = 0; i < height; i++)
+            {
+                gridNodes[i] = new int[width];
+                for (int j = 0; j < width; j++)
+                {
+                    gridNodes[i][j] = gridIndex++;
+                }
+            }
+            return gridNodes;
+        }
 
-    for node in unseenNodes:
-        shortest_distance[node] = infinity
+        public int[][] BlankGrid(int height, int width)
+        {
+            var gridNodes = new int[height][];
+            for (int i = 0; i < height; i++)
+            {
+                gridNodes[i] = new int[width];
+                for (int j = 0; j < width; j++)
+                {
+                    gridNodes[i][j] = 0;
+                }
+            }
+            return gridNodes;
+        }
 
-    shortest_distance[start] = 0
+            public List<int> Dijkstra(List<List<int>> graph, int start, int goal)
+        {
+            return new List<int>();
+        }
 
-    while unseenNodes:
-        minNode = None
-        for node in unseenNodes:
-            if minNode is None:
-                minNode = node
-            elif shortest_distance[node] < shortest_distance[minNode]:
-                minNode = node
+        public int GetCell(int row, int column, int width, int height)
+        {
+            return 0;
+        }
 
-        for childNode, weight in graph[minNode].items():
-            if weight + shortest_distance[minNode] < shortest_distance[childNode]:
-                shortest_distance[childNode] = weight + shortest_distance[minNode]
-                predecessor[childNode] = minNode
+        public (int, int) GetRowCol(int cell, int width, int height)
+        {
+            return (0, 0);
+        }
 
-        unseenNodes.pop(minNode)
+        public void PrintGrid(float[][] grid)
+        {
 
-    currentNode = goal
-    while currentNode != start:
-        try:
-            path.insert(0, currentNode)
-            currentNode = predecessor[currentNode]
-        except:
-            path = None
-            break
+        }
 
-    if path is not None:
-        path.insert(0, start)
+        public void PrintGraph(Dictionary<int, List<int[]>> graph)
+        {
+            foreach (var g in graph)
+            {
+                string output = $"{g.Key}: ";
+                foreach (var v in g.Value)
+                    output += $"{v[0]},";
+                Console.WriteLine(output);
+            }
 
-    return path
-
-def get_cell_num(r, c, shape):
-    return r * shape[1] + c
-
-def get_row_col(cell, shape):
-    r = cell // shape[1]
-    c = cell % shape[1]
-    return r, c
-
-
-print("Maze")
-grid = np.zeros(shape=(50,50), dtype=int)
-print_grid(grid)
-
-graph = build_np_graph(grid)
-
-path = dijkstra(graph, get_cell_num(0,0, grid.shape), get_cell_num(3, 3, grid.shape))
-
-print("Path Taken")
-print(path)
-draw_grid = grid.copy()
-
-for cell in path:
-    
-    position = get_row_col(cell, grid.shape)
-    draw_grid[position] = 2
-    
-print_grid(draw_grid)
+        }
+    }
+}
